0fdfeb8516b67d7dca749ce81cef15840bd0b0e4
Star1X-Dev <abdastar2@gmail.com>
2025-03-21 17:56:58 +0100

    fixed pwa path problems etc..

--- app.js
+++ app.js
@@ -497,18 +497,7 @@ window.addEventListener("load", () => {
     setupPackagingTableListener();
 });
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('SW registered:', registration);
-        })
-        .catch(error => {
-          console.log('SW registration failed:', error);
-        });
-    });
-  }
+
 
   let deferredPrompt;
 
